70cd1757a5b493fdc329b95e9cc0069348654765
Rsuleymanov <Tlnau73291>
2020-04-22 00:08:06 +0300

    Initial commit

---
+++ src/TabMult.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class TabMult {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число ");
+        float i = in.nextFloat();
+        System.out.println("1 x " + i + " = " + i);
+        System.out.println("2 x " + i + " = " + 2*i);
+        System.out.println("3 x " + i + " = " + 3*i);
+        System.out.println("4 x " + i + " = " + 4*i);
+        System.out.println("5 x " + i + " = " + 5*i);
+        System.out.println("6 x " + i + " = " + 6*i);
+        System.out.println("7 x " + i + " = " + 7*i);
+        System.out.println("8 x " + i + " = " + 8*i);
+        System.out.println("9 x " + i + " = " + 9*i);
+        System.out.println("10 x " + i + " = " + 10*i);
+
+    }
+}
